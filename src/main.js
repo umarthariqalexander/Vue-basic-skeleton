287bc7351892cf5dc50b2c48fd4ab996a923a5d3
Umar Thariq <umarthariq@Umars-MacBook-Pro.local>
2019-10-16 15:29:35 +0530

    added BliBli DLS as component dependencies

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Blue from '@blibli/dls'
+import '@blibli/dls/dist/blue.min.css'
+import '@/assets/scss/main.scss'
+
+Vue.use(Blue)
 
 Vue.config.productionTip = false
 
